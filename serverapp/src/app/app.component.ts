af51debec485b6af0b1eb8b668ecc0e134d759ce
Enrico Elizan <eelizanjr@outlook.com>
2024-01-11 17:43:05 -0500

    Adding app component to fe

--- serverapp/src/app/app.component.ts
+++ serverapp/src/app/app.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { catchError, map, startWith } from 'rxjs/operators';
+import { DataState } from './enum/data-state.enum';
+import { Status } from './enum/status.enum';
+import { AppState } from './interface/app-state';
+import { CustomResponse } from './interface/custom-response';
+import { Server } from './interface/server';
 import { ServerService } from './service/server.service';
 
 @Component({
@@ -7,8 +15,27 @@ import { ServerService } from './service/server.service';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  constructor(private serverService: ServerService) {}
+  appState$: Observable<AppState<CustomResponse>>;
 
-  // whenever the page is done initializing ngOninit will execute
-  ngOnInit(): void {}
+  constructor(private serverService: ServerService) {} // dependency injection
+
+  /* SETTING APPLICATION STATE (SERVER DATA) ON INIT */
+  ngOnInit(): void {
+    // after page initializing set the state of the application
+    this.appState$ = this.serverService.servers$.pipe(
+      // map any reposne we get
+      map((response) => {
+        return {
+          dataState: DataState.LOADED_STATE,
+          appData: {
+            response,
+          },
+        };
+      }),
+      startWith({ dataState: DataState.LOADING_STATE }), // pass something to observable in until we get the response back
+      catchError((error: string) => {
+        return of({ dataState: DataState.ERROR_STATE, error });
+      })
+    );
+  }
 }
